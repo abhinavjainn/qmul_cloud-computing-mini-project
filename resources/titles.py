c6ea6f38e69654c8ab275ff6d48529a0305fb694
Abey40M <abeyatta@gmail.com>
2021-03-25 11:31:19 +0000

    Major build

--- resources/titles.py
+++ resources/titles.py
@@ -1,7 +1,9 @@
 
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-from models.title import TitlesModel
+from models.titles import TitlesModel
+
+#
 
 
 class Title(Resource):
