24bf1b934e950ee865fbe87a5f898013dbff5e7b
ABHINAV JAIN <abhinavjain@ABHINAVs-MacBook-Air.local>
2021-03-28 02:43:57 +0100

    Fix get id method

--- models/admin.py
+++ models/admin.py
@@ -7,9 +7,14 @@ class AdminModel(db.Model):
 
     adminid  = db.Column(db.String(40), primary_key=True)
 
-    def __init__(self, username, sid, status):
+    def __init__(self):
         self.adminid = "34fce8e54af2a418da63ce05b265cc8ea98cc1ef"
+        self.save_to_db()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
-    def get_id(self):
-        return self.adminid
+    def get_id(cls):
+        return cls.query.filter_by().first().adminid
