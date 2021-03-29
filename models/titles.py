c5d2ea02075cee79881111da31171690d9cc9424
ABHINAV JAIN <abhinavjain@ABHINAVs-MacBook-Air.local>
2021-03-29 18:50:50 +0100

    fix syntax errors

--- models/titles.py
+++ models/titles.py
@@ -8,7 +8,7 @@ class TitlesModel(db.Model):
     title = db.Column(db.String(50), primary_key=True)
     remarks = db.Column(db.String(80))
 
-    def __init__(self, listname, title, remarks):
+    def __init__(self, listname, title, remarks=None):
         self.listname = listname
         self.title = title
         self.remarks = remarks
@@ -20,9 +20,13 @@ class TitlesModel(db.Model):
     def find_by_listname(cls,listname):
         return cls.query.filter_by(listname=listname).first()
 
+    @classmethod
+    def find_by_listname_title(cls,listname, title):
+        return cls.query.filter_by(listname=listname, title = title).first()        
+
     def save_to_db(self):
         db.session.add(self)
-        db.session.commit()
+        db.session.commit()        
 
     def delete_from_db(self):
         db.session.delete(self)
