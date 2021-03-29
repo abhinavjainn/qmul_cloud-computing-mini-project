de2d430ee34c5c16c4048172d373d053f05cafb9
Abey40M <abeyatta@gmail.com>
2021-03-29 15:59:59 +0100

    delete method done

--- resources/sessions.py
+++ resources/sessions.py
@@ -1,20 +1,23 @@
 import hashlib
 import uuid
 from flask_restful import Resource, reqparse
+from model.user import Username
 
 # Login user
+
+
 class Login(Resource):
     parser = reqparse.RequestParser(bundle_errors=True)
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="Username cannot be blank."
-    )
+                        type=str,
+                        required=True,
+                        help="Username cannot be blank."
+                        )
     parser.add_argument('password',
-        type=str,
-        required=True,
-        help="Password cannot be blank."
-    )  
+                        type=str,
+                        required=True,
+                        help="Password cannot be blank."
+                        )
 
     # Authenticate User
     def post(username, password):
@@ -22,54 +25,45 @@ class Login(Resource):
 
         # Hashed Password
         hash = hashlib.new('ripemd160')
-        hashed_pswd=hash.update(data['password'].encode("utf-8"))
+        hashed_pswd = hash.update(data['password'].encode("utf-8"))
 
         user = UserModel.query.filter_by(username=username).first()
         if user is None:
             return {"message": "Username or Password is incorrect"}
         elif hashed_pswd.hexdigest() != user.password:
-            return{"message" : "Username or Password is incorrect"}
+            return{"message": "Username or Password is incorrect"}
         else:
             session_id = uuid.uuid4()
-            data['sid']=session_id
-        
+            data['sid'] = session_id
+
         # Save Session_id to DB
             session = SessionModel(**data)
             session.save_to_db()
-            
-            return {"message" : "User successfully logged in and the Session id :{}".format(session_id)}, 201
+
+            return {"message": "User successfully logged in and the Session id :{}".format(session_id)}, 201
 
 
 # Logout User
 class Logout(Resource):
     parser = reqparse.RequestParser(bundle_errors=True)
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="Username cannot be blank."
-    )
+                        type=str,
+                        required=True,
+                        help="Username cannot be blank."
+                        )
     parser.add_argument('session_id',
-        type=str,
-        required=True,
-        help="Session_id cannot be blank."
-    )  
+                        type=str,
+                        required=True,
+                        help="Session_id cannot be blank."
+                        )
 
     def delete_user(self):
         data = Logout.parser.parse_args()
-        user = SessionModel.query.filter_by(sid=data['session_id'], status='Active', username=data['username'] ).first()
+        user = SessionModel.query.filter_by(
+            sid=data['session_id'], status='Active', username=data['username']).first()
         if not user:
-            return {'message' : 'No active session found'}, 404
+            return {'message': 'No active session found'}, 404
         else:
             db.session.delete(session_id)
             db.session.commit()
-            return {'message' : 'User logged out!'}, 200
-
-        
-
-    
-
-
-
-
-
-
+            return {'message': 'User logged out!'}, 200
--- resources/titles.py
+++ resources/titles.py
@@ -59,9 +59,9 @@ class CreateList(Resource):
 
     def post(self):
         data = CreateList.parser.parse_args()
-#       Check if user session is valid
+        # Check if user session is valid
         # TBD####
-#       Check if user already has a list, if yes => error
+        # Check if user already has a list, if yes => error
         if UserModel.find_by_user_and_list(data['username'], data["listname"]):
             return {"message": "A listname already exists for the user"}, 400
 
@@ -70,7 +70,7 @@ class CreateList(Resource):
         titles.save_to_db()
 
 
-class AddList(Resource):
+class AddToList(Resource):
     parser = reqparse.RequestParser()
 
     parser.add_argument('username',
@@ -104,20 +104,24 @@ class AddList(Resource):
 
     def put(self):
         data = AddtoList.parser.parse_args()
-#       Check if user session is valid
-#       Check if user already has a list, if yes => error
-        if UserModel.find_by_user_and_list(data['username'], data["listname"]):
-            return {"message": "A listname already exists for the user"}, 400
         # check if list name exists, if not create one with required parameters
-
         item = TitlesModel.find_by_listname(data['listname'])
-        if item is None:
-            item = TitlesModel(data["listname"], data["title"], data["remarks"])
+        # Check if user session is valid
+        session_id_valid = SessionModel(data['sid'])
+        # if user doesn't have a valid active sessions
+        if session_id_valid is None:
+            return {'message': 'Inter a valid session ID'}, 404
+        else:
+            # Check if user already has a list, if yes => error
+            if UserModel.find_by_user_and_list(data['username'], data["listname"]):
+                return {"message": "A listname already exists for the user"}, 404
+            if item is None:
+                item = TitlesModel(data["listname"], data["title"], data["remarks"])
+                return {'message': 'Resource updated successfully'}, 204
+                item.save_to_db()
 
-        item.save_to_db()
 
-
-class DeleteList(Resource):
+class DeleteFromList(Resource):
     parser = reqparse.RequestParser()
 
     parser.add_argument('username',
@@ -125,7 +129,11 @@ class DeleteList(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
+    parser.add_argument('sid',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
     parser.add_argument('listname',
                         type=str,
                         required=True,
@@ -137,107 +145,64 @@ class DeleteList(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-    parser.add_argument('remarks',
+
+    def delete_from_list(self):
+        data = DeliteList.parser.parse_args()
+        # title to delete
+        list_to_delete = TitlesModel.find_by_listname_and_title(data['username'], data["title"])
+        # Check if user session is valid
+        valid_session_id = SessionModel(data['sid'])
+        # if user doesn't have a valid active sessions, error
+        if valid_session_id is None:
+            return {'message': 'No active sessions found'}, 404
+        else:
+            if TitlesModel(data['title']) not in list_to_delete:  # len(list_to_delete) == 0:
+                return {'message': 'The requested title doesnt exist in the list'}, 400
+            else:
+                TitlesModel(data['title']).delete_from_db()
+                # TitlesModel(data['title']).delete_from_db()
+                return {'message': 'List has been deleted from the database'}, 200
+
+
+class DeleteList(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('username',
                         type=str,
-                        required=False,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('sid',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('listname',
+                        type=str,
+                        required=True,
                         help="This field cannot be left blank!"
                         )
 
-    def delete(self):
+    def delete_list(self):
         data = DeliteList.parser.parse_args()
+        # list to delete
+        list_to_delete = UserModel.find_by_username(data['listname'])
         # Check if user session is valid
-        item_to_delete = []
-        item_to_delete.append(UserModel.find_by_user_and_list(data['username'], data["listname"]))
-        item_to_delete.append(data['title'])
-        if len(item_to_delete) == 0:
-            return {'message': 'username and listname dont exist'}, 400
+        valid_session_id = SessionModel(data['sid'])
+        # if user doesn't have a valid active sessions, error
+        if session_id_valid is None:
+            return {'message': 'No active sessions found'}, 404
         else:
-            item_to_delete.delete_from_db()
-            # TitlesModel(data['title']).delete_from_db()
-
-        return {'message': 'Title deleted'}
-
-# class Title(Resource):
-#     parser = reqparse.RequestParser()
-
-#     parser.add_argument('username',
-#                         type=String,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-
-#     parser.add_argument('sid',
-#                         type=Str,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-#     parser.add_argument('title',
-#                         type=str,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-#     parser.add_argument('remarks',
-#                         type=str,
-#                         required=True,
-#                         help="Please give remarks."
-#                         )
-
-#     # @jwt_required()
-#     # user, session id
-#     def get(self, listname):
-#         data = Title.parser.parse_args()
-#         item = TitlesModel.find_by_listname(listname)
-#         if len(item) == 0:
-#             return {"error": 'Title name not found'}, 404
-#         else:
-#             if SessionModel.find_by_sid(data['sid']) and UserModel.find_by_username(data['username']):
-#                 return item  # .jason()
-
-#     # replace multiple time not allowed!
-#     def post(self, listname):
-#         data = Title.parser.parse_args()
-#         item = TitlesModel.find_by_listname(listname)
-
-#         if TitlesModel.find_by_listname(item):
-#             return {'message': "A title with listname '{}' already exists.".format(item)}, 400
-
-#         item = TitlesModel(listname, **data)
-
-#         try:
-#             item.save_to_db()
-#         except:
-#             return {"message": "An error occurred inserting the title."}, 500
-
-#         return item.json(), 201
-#     # delete item from list: user, session id, title
-
-#     def delete(self, litsname):
-#         item = TitlesModel.find_by_name(listname)
-
-#         if titles:
-#             item.delete_from_db()
-
-#         return {'message': 'Title deleted'}
-
-#     # add item to list: user, session id, title
-#     # replace multiple time no problem! hence idempotent
-#     def put(self, listname):
-#         data = Title.parser.parse_args()
-#         item = TitlesModel.find_by_name(listname)
-#         #sess = SessionModel.find_by_sid()
-
-#         if item is None:
-#             item = TitlesModel(listname, **data)
-#         else:
-#             item.listname = data['listname']
-#             item.remarks = data['remarks']
-#             item.title = data['title']
-
-#         item.save_to_db()
-
-#         return item.json()
+            if len(list_to_delete) == 0:
+                return {'message': 'The request item doesnt exist in the database'}, 400
+            else:
+                # delete title from user
+                List = UserModel(data['listname'])
+                List.delete_from_db()
+                # TitlesModel(data['title']).delete_from_db()
+                return {'message': 'List has been deleted successfully'}, 200
 
 
-# class TitlesList(Resource):
-#     def get(self):
-#         return {'titles': [x.json() for x in TitlesModel.query.all()]}
+class TitlesList(Resource):
+    def get(self):
+        return {'titles': [x.json() for x in TitlesModel.query.all()]}
